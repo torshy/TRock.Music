f6964b200913424ff004077299fa3cc72898b7a0
Torstein Auensen <torshy@gmail.com>
2012-10-09 21:22:29 +0200

    Added two ISongStream implementations

---
+++ src/TRock.Music/MultiSongStream.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace TRock.Music
+{
+    public class MultiSongStream : ISongStream
+    {
+        #region Fields
+
+        private readonly IEnumerable<Song> _songs;
+        private int _counter;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public MultiSongStream(IEnumerable<Song> songs)
+        {
+            _songs = songs;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<Song> Current
+        {
+            get
+            {
+                if (_counter == 1)
+                {
+                    return _songs;
+                }
+
+                return new Song[0];
+            }
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        public bool MoveNext(CancellationToken token)
+        {
+            if (Interlocked.Increment(ref _counter) == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion Methods
+    }
+}
---
+++ src/TRock.Music/SingleSongStream.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace TRock.Music
+{
+    public class SingleSongStream : ISongStream
+    {
+        #region Fields
+
+        private readonly Song _song;
+        private int _counter;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public SingleSongStream(Song song)
+        {
+            _song = song;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<Song> Current
+        {
+            get
+            {
+                if (_counter == 1)
+                {
+                    return new[] { _song };
+                }
+
+                return new Song[0];
+            }
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        public bool MoveNext(CancellationToken token)
+        {
+            if (Interlocked.Increment(ref _counter) == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion Methods
+    }
+}
