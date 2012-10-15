415b023d6ee82103f23f5c8010c71c33179c0ee9
Torstein Auensen <torshy@gmail.com>
2012-10-15 17:55:55 +0200

    Added a couple of tests

--- src/TRock.Music.Tests/AggregateSongProviderTest.cs
+++ src/TRock.Music.Tests/AggregateSongProviderTest.cs
@@ -1,8 +1,10 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
-using TRock.Music.Grooveshark;
-using TRock.Music.Spotify;
+using NSubstitute;
 
 using Xunit;
 
@@ -10,15 +12,61 @@ namespace TRock.Music.Tests
 {
     public class AggregateSongProviderTest
     {
+        #region Methods
+
         [Fact]
-        public async Task DoIt()
+        public void GetSongs_ExceptionBubblesUp()
         {
             var provider = new AggregateSongProvider();
-            provider.Providers.Add(new SpotifySongProvider(new DefaultSpotifyImageProvider()));
-            provider.Providers.Add(new GroovesharkSongProvider(new GroovesharkClientWrapper()));
 
-            var songs = await provider.GetSongs("NOFX", CancellationToken.None);
-            Assert.NotEmpty(songs);
+            var provider1 = Substitute.For<ISongProvider>();
+            var provider2 = Substitute.For<ISongProvider>();
+            provider.Providers.Add(provider1);
+            provider.Providers.Add(provider2);
+
+            provider1
+                .GetSongs(Arg.Any<string>(), Arg.Any<CancellationToken>())
+                .Returns(x => { throw new Exception(); });
+
+            provider2
+                .GetSongs(Arg.Any<string>(), Arg.Any<CancellationToken>())
+                .Returns(x =>
+                {
+                    return Task<IEnumerable<Song>>.Factory.StartNew(() => new[] { new Song(), new Song() });
+                });
+
+            Assert.Throws<AggregateException>(() => provider.GetSongs("NOFX", CancellationToken.None).Wait());
         }
+
+        [Fact]
+        public void GetSongs_AllSongsFromEveryProviderReturned()
+        {
+            var provider = new AggregateSongProvider();
+
+            var provider1 = Substitute.For<ISongProvider>();
+            var provider2 = Substitute.For<ISongProvider>();
+            provider.Providers.Add(provider1);
+            provider.Providers.Add(provider2);
+
+            provider1
+                .GetSongs(Arg.Any<string>(), Arg.Any<CancellationToken>())
+                .Returns(x =>
+                {
+                    return Task<IEnumerable<Song>>.Factory.StartNew(() => new[] { new Song(), new Song() });
+                });
+
+            provider2
+                .GetSongs(Arg.Any<string>(), Arg.Any<CancellationToken>())
+                .Returns(x =>
+                {
+                    return Task<IEnumerable<Song>>.Factory.StartNew(() => new[] { new Song(), new Song() });
+                });
+
+            var result = provider.GetSongs("NOFX", CancellationToken.None).Result;
+
+            Assert.Equal(4, result.Count());
+        }
+
+        #endregion Methods
     }
 }
