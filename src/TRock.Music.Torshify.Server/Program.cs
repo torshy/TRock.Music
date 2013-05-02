fec08b91bca18727ec4e5e7cad2000af93737c08
Torstein Auensen <torshy@gmail.com>
2013-05-02 19:04:42 +0200

    Added SetErrorMode in Torshify Server, so it will die silently

--- src/TRock.Music.Torshify.Server/Program.cs
+++ src/TRock.Music.Torshify.Server/Program.cs
@@ -1,23 +1,43 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace TRock.Music.Torshify.Server
 {
     class Program
     {
+        #region Enumerations
+
+        [Flags]
+        public enum ErrorModes : uint
+        {
+            SYSTEM_DEFAULT = 0x0,
+            SEM_FAILCRITICALERRORS = 0x0001,
+            SEM_NOALIGNMENTFAULTEXCEPT = 0x0004,
+            SEM_NOGPFAULTERRORBOX = 0x0002,
+            SEM_NOOPENFILEERRORBOX = 0x8000
+        }
+
+        #endregion Enumerations
+
         #region Methods
+
+        [DllImport("kernel32.dll")]
+        static extern ErrorModes SetErrorMode(ErrorModes uMode);
 
         static void Main(string[] args)
         {
             EnsureProcessIsViableSpotifyHost();
+            var errorMode = SetErrorMode(ErrorModes.SYSTEM_DEFAULT);
+            SetErrorMode(errorMode | ErrorModes.SEM_NOGPFAULTERRORBOX);
 
             AppDomain.CurrentDomain.AssemblyResolve += (sender, e) =>
             {
                 String resourceName = "TRock.Music.Torshify.Server.Libs." + new AssemblyName(e.Name).Name + ".dll";
                 using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
                 {
-                    Byte[] assemblyData = new Byte[stream.Length];
+                    byte[] assemblyData = new Byte[stream.Length];
                     stream.Read(assemblyData, 0, assemblyData.Length);
                     return Assembly.Load(assemblyData);
                 }
